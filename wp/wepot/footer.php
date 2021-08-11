982fde8d541e0dc44d5f558e8791dcbf0628d53b
petaBite-yo <yosuke@d-petabite.com>
2021-08-12 02:35:27 +0900

    【php修正】フッターにfacebookのリンクを差し込み

--- wp/wepot/footer.php
+++ wp/wepot/footer.php
@@ -66,7 +66,7 @@
                         <ul class="link-sns">
                             <li class="link-sns_item"><a href="https://twitter.com/wepot_media" target="_blank" rel=“noopener”><img src="<?php echo get_template_directory_uri(); ?>/img/icon/twitter.png" alt=""></a>
                             </li>
-                            <li class="link-sns_item"><a href="/" target="_blank" rel=“noopener”><img src="<?php echo get_template_directory_uri(); ?>/img/icon/facebook.png" alt=""></a>
+                            <li class="link-sns_item"><a href="https://www.facebook.com/Wepot-102609692133115" target="_blank" rel=“noopener”><img src="<?php echo get_template_directory_uri(); ?>/img/icon/facebook.png" alt=""></a>
                             </li>
                             <li class="link-sns_item"><a href="https://www.linkedin.com/in/wepot/" target="_blank" rel=“noopener”><img src="<?php echo get_template_directory_uri(); ?>/img/icon/linkedin.png" alt=""></a>
                             </li>
