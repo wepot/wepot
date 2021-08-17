9a9a1e8dcff24d0e46a41aab7a735e372fe8983f
petaBite-yo <yosuke@d-petabite.com>
2021-08-17 19:05:36 +0900

    【php修正】「おわり」を「終わり」に変更

--- wp/wepot/page-contact.php
+++ wp/wepot/page-contact.php
@@ -6,7 +6,7 @@
             <header class="section_heading">
                 <h1 class="section_title">お問い合わせ</h1>
             </header>
-            <p class="section_text">Wepotは個々に違う強みを持ったクリエイティブチームです。お客様の課題解決に最適なメンバーアサインでスムーズなプロジェクト進行をいたします。制作しておわりではなく、お客様が求める成果に結びつくご提案をさせていただきます。</p>
+            <p class="section_text">Wepotは個々に違う強みを持ったクリエイティブチームです。お客様の課題解決に最適なメンバーアサインでスムーズなプロジェクト進行をいたします。制作して終わりではなく、お客様が求める成果に結びつくご提案をさせていただきます。</p>
             <div class="can contactPage-can">
                 <p class="can_text02">まずは、お気軽にご相談ください</p>
                 <ul class="can_list">
