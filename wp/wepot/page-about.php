b14a0aa327c5851dbf99d91e27b5291212a6a861
petaBite-yo <yosuke@d-petabite.com>
2021-10-13 10:23:54 +0900

    【php修正】page-about.phpのimgタグにwidthとheight値を記述

--- wp/wepot/page-about.php
+++ wp/wepot/page-about.php
@@ -22,49 +22,49 @@
             <div class="categoryCards">
                 <ul class="categoryCards_list">
                     <li class="categoryCards_item">
-                        <div class="categoryCards_img"><img src="<?php echo get_template_directory_uri(); ?>/img/about/about-img01.jpg" alt="アイコン"></div>
+                        <div class="categoryCards_img"><img src="<?php echo get_template_directory_uri(); ?>/img/about/about-img01.jpg" alt="アイコン" width="384" height="216"></div>
                         <h3 class="categoryCards_title">Webデザイン</h3>
                         <p class="categoryCards_text">
                             Webデザインに関するあらゆる記事を執筆します。新規Webサイト制作やWebサイトのリニューアル、採用サイト、ブランディングやキャンペーン用など、サイトの目的に応じて重要な要素は異なります。効果的なWebデザインを制作するために大事なTipsを記事にしています。
                         </p>
                     </li><!-- /.categoryCards_item -->
                     <li class="categoryCards_item">
-                        <div class="categoryCards_img"><img src="<?php echo get_template_directory_uri(); ?>/img/about/about-img02.jpg" alt="アイコン"></div>
+                        <div class="categoryCards_img"><img src="<?php echo get_template_directory_uri(); ?>/img/about/about-img02.jpg" alt="アイコン" width="384" height="216"></div>
                         <h3 class="categoryCards_title">WordPress</h3>
                         <p class="categoryCards_text">
                             WordPressに関する情報は数多く存在していますが、有名なツールであるからこそセキュリティや運用を考えた設計が必要です。WepotではWordPressの基本的な情報から運用のしやすい設計とセキュリティ対策の重要性などについて発信します。
                         </p>
                     </li><!-- /.categoryCards_item -->
                     <li class="categoryCards_item">
-                        <div class="categoryCards_img"><img src="<?php echo get_template_directory_uri(); ?>/img/about/about-img03.jpg" alt="アイコン"></div>
+                        <div class="categoryCards_img"><img src="<?php echo get_template_directory_uri(); ?>/img/about/about-img03.jpg" alt="アイコン" width="384" height="216"></div>
                         <h3 class="categoryCards_title">マーケティング</h3>
                         <p class="categoryCards_text">
                             どの分野・業界においても必要な「商品やサービスをユーザーに届ける」こと、それがマーケティングです。ターゲットの調査・分析、市場の動向、デジタルの進化、SNS活用など、さまざまな要素の掛け合わせで最適解を出すことが大切です。マーケティングの基本的な考え方から応用的な知識までお届けします。
                         </p>
                     </li><!-- /.categoryCards_item -->
                     <li class="categoryCards_item">
-                        <div class="categoryCards_img"><img src="<?php echo get_template_directory_uri(); ?>/img/about/about-img04.jpg" alt="アイコン"></div>
+                        <div class="categoryCards_img"><img src="<?php echo get_template_directory_uri(); ?>/img/about/about-img04.jpg" alt="アイコン" width="384" height="216"></div>
                         <h3 class="categoryCards_title">ディレクション</h3>
                         <p class="categoryCards_text">
                             ディレクションに関する情報をお届けします。プロジェクトに欠かすことのできないディレクターの業務。言語化が難しいが故に体系的にまとめているメディアが少ないのが実情です。当メディアでは必要な基礎知識から資料作成、マネジメントや品質・進行管理まで、幅広い領域の情報を網羅しています。
                         </p>
                     </li><!-- /.categoryCards_item -->
                     <li class="categoryCards_item">
-                        <div class="categoryCards_img"><img src="<?php echo get_template_directory_uri(); ?>/img/about/about-img05.jpg" alt="アイコン"></div>
+                        <div class="categoryCards_img"><img src="<?php echo get_template_directory_uri(); ?>/img/about/about-img05.jpg" alt="アイコン" width="384" height="216"></div>
                         <h3 class="categoryCards_title">コーディング</h3>
                         <p class="categoryCards_text">
                             コーディングに関する専門的な情報は多く発信されていますが、Wepotではコーディングについて、できることや重要性をよりわかりやすく言語化しています。SEOに求められる設計に必要なコーディングの大事な情報をお届けします。
                         </p>
                     </li><!-- /.categoryCards_item -->
                     <li class="categoryCards_item">
-                        <div class="categoryCards_img"><img src="<?php echo get_template_directory_uri(); ?>/img/about/about-img06.jpg" alt="アイコン"></div>
+                        <div class="categoryCards_img"><img src="<?php echo get_template_directory_uri(); ?>/img/about/about-img06.jpg" alt="アイコン" width="384" height="216"></div>
                         <h3 class="categoryCards_title">イラスト</h3>
                         <p class="categoryCards_text">
                             イラストは物事をわかりやすく伝え、世界観を作り出すことが得意なクリエイティブです。イラスト制作現場の基礎知識からブランディングに役立つイラストの活用方法などをお届けしていきます。
                         </p>
                     </li><!-- /.categoryCards_item -->
                     <li class="categoryCards_item">
-                        <div class="categoryCards_img"><img src="<?php echo get_template_directory_uri(); ?>/img/about/about-img07.jpg" alt="アイコン"></div>
+                        <div class="categoryCards_img"><img src="<?php echo get_template_directory_uri(); ?>/img/about/about-img07.jpg" alt="アイコン" width="384" height="216"></div>
                         <h3 class="categoryCards_title">動画編集</h3>
                         <p class="categoryCards_text">
                             動画/映像編集・動画広告など、今の時代に動画クリエイティブは欠かせない存在となりました。新規顧客獲得などの広告として、多くの人から共感を得るストーリーの話し手として、人の行動を変えるきっかけとして。それらが得意な動画を使い、お客様のビジネス課題の解決に役立つ情報をお届けします。
