51e1cf7fb345c0ba04acd8e2de0a8202dd6c3ba9
petaBite-yo <yosuke@d-petabite.com>
2021-11-01 17:32:32 +0900

    【php修正】サイドバーにAdSense広告を表示

--- wp/wepot/sidebar.php
+++ wp/wepot/sidebar.php
@@ -24,4 +24,12 @@
         </div><!-- /.sideMenu_list -->
     </div><!-- /.sideMenu_wrap -->
     <?php dynamic_sidebar('side-widget'); ?>
+    <div class="sideMenu_wrap">
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5967057474234828" crossorigin="anonymous"></script>
+        <!-- サイドバー用広告 -->
+        <ins class="adsbygoogle" style="display:inline-block;width:300px;height:250px" data-ad-client="ca-pub-5967057474234828" data-ad-slot="4693798253"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div><!-- /.sideMenu_wrap -->
 </aside>
